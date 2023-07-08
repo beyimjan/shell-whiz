a195196fe69c1edc1766abf7ddab0edc97c58441
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-07-08 19:54:51 +0600

    Refactor code

--- shell_whiz/__main__.py
+++ shell_whiz/__main__.py
@@ -6,69 +6,96 @@ import subprocess
 import openai
 
 DELIMITER = "####"
+SHELL = "bash"
 
-NL_TO_BASH_SYSTEM_MESSAGE = f"""You are a Bash command translator. Your role is to translate natural language into a Bash command. Think that all necessary programs are installed.
+NL_TO_SHELL_COMMAND_PMT = """You are a {SHELL} command translator. Your role is to translate natural language into a {SHELL} command. Think that all necessary programs are installed.
 
-Provide only a ready-to-execute command. Do not write explanations.
+Provide only a ready-to-execute command. Do not write any explanation.
 
 Queries will be separated by {DELIMITER} characters."""
 
 
-def translate_natural_language_to_bash(query):
-    chain_of_thoughts = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0.35,
-        messages=[
-            {"role": "system", "content": NL_TO_BASH_SYSTEM_MESSAGE},
-            {
-                "role": "user",
-                "content": f"{DELIMITER}{query}{DELIMITER}",
-            },
-        ],
-    )
+class ShellWhizTranslationError(Exception):
+    pass
 
-    result_in_json = openai.ChatCompletion.create(
+
+def extract_shell_command(haystack):
+    extracted_json = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=0,
+        max_tokens=256,
         messages=[
             {
                 "role": "system",
-                "content": 'Extract JSON from user messages with a single key "bash_command".\n\nOnly generate JSON to make your output machine readable.',
+                "content": 'Extract JSON from user messages with a single key "shell_command". Only generate JSON to make your output machine readable.',
             },
             {
                 "role": "user",
-                "content": chain_of_thoughts.choices[0].message["content"],
+                "content": f"{DELIMITER}\n{haystack}\n{DELIMITER}",
             },
         ],
     )
-    try:
-        result_in_json = json.loads(
-            result_in_json.choices[0].message["content"]
-        )
-    except JSONDecodeError:
-        raise Exception("Could not decode JSON.")
 
-    return result_in_json.get("bash_command", None)
+    try:
+        extracted_json = json.loads(
+            extracted_json.choices[0].message["content"]
+        )
+        extracted_shell_command = extracted_json.get("shell_command", None)
+    except JSONDecodeError:
+        raise ShellWhizTranslationError()
+
+    return extracted_shell_command
+
+
+def translate_natural_language_to_shell_command(query):
+    translation = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0.35,
+        max_tokens=256,
+        messages=[
+            {
+                "role": "system",
+                "content": NL_TO_SHELL_COMMAND_PMT.format(
+                    SHELL=SHELL, DELIMITER=DELIMITER
+                ),
+            },
+            {
+                "role": "user",
+                "content": f"{DELIMITER}\n{query}\n{DELIMITER}",
+            },
+        ],
+    )
+
+    # Translation sometimes contains not only the commmand
+    shell_command = extract_shell_command(
+        translation.choices[0].message["content"]
+    )
+
+    return shell_command
 
 
 def main():
     query = sys.argv[1:]
     if not query:
-        print("Please provide a query.")
+        print("Error: Please provide a query.", file=sys.stderr)
         sys.exit(1)
 
     try:
-        command = translate_natural_language_to_bash(" ".join(query))
+        shell_command = translate_natural_language_to_shell_command(
+            " ".join(query)
+        )
 
-        print(f"Command: {command}")
+        print(f"Command: {shell_command}")
 
         yes_no = input("Execute command? [y/N] ")
         if yes_no.lower() == "y":
-            subprocess.run(command, shell=True)
+            subprocess.run(shell_command, shell=True)
     except openai.OpenAIError:
-        print("Could not connect to OpenAI API.")
-    except Exception:
-        print("Something went wrong.")
+        print("Could not connect to OpenAI API.", file=sys.stderr)
+        sys.exit(2)
+    except ShellWhizTranslationError:
+        print("ShellWhiz doesn't understand your query.", file=sys.stderr)
+        sys.exit(3)
 
 
 if __name__ == "__main__":
