d7fcb0a3ffd24fcbe95cc04635be7937bfc6aefa
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-09-18 04:29:58 +0600

    Use function calling to generate commands

---
+++ shell_whiz/jsonschemas.py
@@ -0,0 +1,10 @@
+translation_json_schema = {
+    "shell_command": {
+        "type": "string",
+        "description": "Shell command to perform the task",
+    },
+    "invalid_request": {
+        "type": "boolean",
+        "description": "Set to true if you cannot perform the task in the command line for any reason",
+    },
+}
