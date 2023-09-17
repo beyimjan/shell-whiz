99a30badc130eaeb5d3577e40308e0bece594080
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-09-18 05:12:33 +0600

    Use function to recognize dangerous command

--- shell_whiz/jsonschemas.py
+++ shell_whiz/jsonschemas.py
@@ -1,10 +1,26 @@
 translation_json_schema = {
-    "shell_command": {
-        "type": "string",
-        "description": "Shell command to perform the task",
+    "type": "object",
+    "properties": {
+        "shell_command": {
+            "type": "string",
+            "description": "Shell command to perform the task",
+        },
+        "invalid_request": {
+            "type": "boolean",
+            "description": "Set to true if you cannot perform the task in the command line for any reason",
+        },
     },
-    "invalid_request": {
-        "type": "boolean",
-        "description": "Set to true if you cannot perform the task in the command line for any reason",
+}
+
+
+dangerous_command_json_schema = {
+    "type": "object",
+    "properties": {
+        "dangerous_to_run": {"type": "boolean"},
+        "dangerous_consequences": {
+            "type": "string",
+            "description": "Brief explanation of the potential side effects of running the command. No more than 12 words.",
+        },
     },
+    "required": ["dangerous_to_run"],
 }
